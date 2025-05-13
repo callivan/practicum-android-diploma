ca156ec2a637864efaea2b4cc7fa50457cd26240
callivan <ivan-my-job@mail.ru>
2025-05-13 21:04:17 +0700

    feature #7 colors and theme

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -1,7 +1,12 @@
 package ru.practicum.android.diploma.ui.root
 
+import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsetsController
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
 
@@ -9,6 +14,7 @@ class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_root)
+        setupThemeAndStatusBar()
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
@@ -18,4 +24,41 @@ class RootActivity : AppCompatActivity() {
         // ...
     }
 
+    private fun setupThemeAndStatusBar() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+    }
+
+    private fun syncStatusBarWithAppTheme() {
+        val isDarkTheme =
+            resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK === Configuration.UI_MODE_NIGHT_YES
+
+        when {
+            // For Android 11+ (API 30+)
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> {
+                window.insetsController?.let {
+                    val appearance = if (isDarkTheme) 0 else WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+
+                    it.setSystemBarsAppearance(appearance, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)
+                }
+            }
+            // For Android 6+ (API 23+)
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.M -> {
+                window.decorView.systemUiVisibility = if (isDarkTheme) {
+                    window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+                } else {
+                    window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                }
+            }
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        syncStatusBarWithAppTheme()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        syncStatusBarWithAppTheme()
+    }
 }
