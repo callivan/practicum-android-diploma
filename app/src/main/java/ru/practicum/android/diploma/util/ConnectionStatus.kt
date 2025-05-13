a05c84aa6379d16067a5a238dac4d62cd55144ee
Viraremi <74133988+Viraremi@users.noreply.github.com>
2025-05-13 11:02:02 +0400

    fix detekt reports

--- app/src/main/java/ru/practicum/android/diploma/util/ConnectionStatus.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/ConnectionStatus.kt
@@ -5,15 +5,15 @@ import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 
 fun isConnected(context: Context): Boolean {
-    val connectivityManager = context.getSystemService(
-        Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+    var result = false
     if (capabilities != null) {
         when {
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> return true
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> return true
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> return true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> result = true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> result = true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> result = true
         }
     }
-    return false
+    return result
 }
