a8b331d3fc0eeb1e974f7fc071e687f7c462f389
Viraremi <74133988+Viraremi@users.noreply.github.com>
2025-05-15 15:56:53 +0400

    Граф навигации

--- app/src/main/java/ru/practicum/android/diploma/ui/filter/FragmentFilter.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/FragmentFilter.kt
@@ -1,24 +1,17 @@
 package ru.practicum.android.diploma.ui.filter
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentFilter.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FragmentFilter : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -31,7 +24,8 @@ class FragmentFilter : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
@@ -39,16 +33,7 @@ class FragmentFilter : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentFilter.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
+
         fun newInstance(param1: String, param2: String) =
             FragmentFilter().apply {
                 arguments = Bundle().apply {
