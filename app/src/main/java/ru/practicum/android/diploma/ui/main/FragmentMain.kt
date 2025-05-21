41ef4ef312157ec3462df594df25073aec3b77c5
vixisss <shkanovaviktoria@mail.ru>
2025-05-21 22:57:59 +0400

    feature 38. экран избранного, без избранного

--- app/src/main/java/ru/practicum/android/diploma/ui/main/FragmentMain.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/main/FragmentMain.kt
@@ -75,7 +75,11 @@ class FragmentMain : Fragment() {
             addTextChangedListener(viewModel.getTextWatcher())
             addTextChangedListener(object : TextWatcher {
                 override fun afterTextChanged(s: Editable?) {
-                    binding.editTextInput.clearIcon.isVisible = !s.isNullOrEmpty()
+                    if(!s.isNullOrEmpty()){
+                        binding.editTextInput.clearIcon.isVisible = true
+                    } else{
+                        binding.editTextInput.clearIcon.isVisible = false
+                    }
                 }
                 override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
                 override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
