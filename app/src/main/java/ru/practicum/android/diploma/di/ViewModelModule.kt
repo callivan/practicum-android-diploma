4da33da12fd22e372457761cb9bea614f8766eec
callivan <ivan-my-job@mail.ru>
2025-05-25 13:12:28 +0700

    Feature 78

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -5,6 +5,7 @@ import org.koin.dsl.module
 import ru.practicum.android.diploma.domain.models.FavoriteVacanciesInteractor
 import ru.practicum.android.diploma.domain.models.VacanciesInteractor
 import ru.practicum.android.diploma.presentation.favorite.FavoriteViewModule
+import ru.practicum.android.diploma.presentation.filter.industry.FilterIndustryViewModel
 import ru.practicum.android.diploma.presentation.main.MainViewModel
 import ru.practicum.android.diploma.presentation.vacancy.VacancyViewModel
 
@@ -19,5 +20,9 @@ val viewModelModule = module {
 
     viewModel<FavoriteViewModule> {
         FavoriteViewModule(get<FavoriteVacanciesInteractor>())
+    }
+
+    viewModel<FilterIndustryViewModel> {
+        FilterIndustryViewModel(get<VacanciesInteractor>())
     }
 }
